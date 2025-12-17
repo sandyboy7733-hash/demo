27eda717b02af3fa963b4a3e7d66aa1283050467
MUKILAN S <sandyboy7733@gmail.com>
2025-12-17 21:20:56 +0530

    1

---
+++ q2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class q2 {
+   q2() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      int var2 = var1.nextInt();
+      if (var2 % 2 == 0) {
+         System.out.println("even");
+      } else {
+         System.out.println("odd");
+      }
+
+   }
+}
